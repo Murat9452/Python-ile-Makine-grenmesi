a99f2f8a314731095e98af77feba3869f619caf1
MURAT YILMAZ <155084043+Murat9452@users.noreply.github.com>
2024-11-02 22:39:08 +0300

    Add files via upload

---
+++ untitled4.py
@@ -0,0 +1,70 @@
+#1.kütüphaneler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# veri yükleme
+veriler = pd.read_csv(r"C:\Users\murat\Desktop\YPZ\maaslar.csv")
+print(veriler) 
+
+
+# data frame dilimleme(slice)
+x= veriler.iloc[:,1:2]
+y= veriler.iloc[:,2:]
+
+# Numpy dizi (array) dönüşümü
+X = x.values
+Y = y.values
+
+# Linear Regression
+# Doğrusal model oluşturma
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, Y)
+
+# Görselleştirme
+plt.scatter(X, Y,color="red")
+plt.plot(x, lin_reg.predict(X),color="blue") 
+plt.show()
+
+
+# Polynomal Regression
+# Doğrusal olmayan (nonlinear model oluşturma)
+# 2. dereceden polinom
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+x_poly = poly_reg.fit_transform(X)
+print(x_poly)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly,y)
+
+# 4. dereceden polinom
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg3 = PolynomialFeatures(degree = 4)
+x_poly3 = poly_reg3.fit_transform(X)
+print(x_poly3)
+lin_reg3 = LinearRegression()
+lin_reg3.fit(x_poly3,y)
+
+
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,lin_reg3.predict(poly_reg3.fit_transform(X)), color = 'blue')
+plt.show() 
+
+
+print(lin_reg.predict([[11]]))
+print(lin_reg.predict([[6.6]]))
+
+print(lin_reg2.predict(poly_reg.fit_transform([[6.6]])))
+print(lin_reg2.predict(poly_reg.fit_transform([[11]]))) 
+
+
+
+
+
+
+
+
+
+
